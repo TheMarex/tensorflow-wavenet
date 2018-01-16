e7b43fc1430e56ede6deddcbdeac30bc821d4276
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-01-16 15:32:54 +0000

    set batch size to 16

--- train_hiphop.sh
+++ train_hiphop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_6_2048 --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True
+python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_6_2048 --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True --batch_size=16
