775ce44bb022492cdb253a979ecc7a8150b5d3d6
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-01-16 23:55:00 +0000

    Add histogram support

--- train_hiphop.sh
+++ train_hiphop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_6_2048 --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True --batch_size=1
+python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_6_2048 --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True --batch_size=1 --silence_threshold=0.1 --histograms=True
+
