e6e7f54fd263a02ae9dd6e2d955c3071867ef5a4
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-01-24 14:13:51 +0000

    Fix hiphop training to onehot 3 layer

--- train_hiphop.sh
+++ train_hiphop.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-#python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_6_2048 --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True --batch_size=1 --silence_threshold=0.1 --histograms=True
-python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_3_4096_real --checkpoint_every=5000 --max_checkpoints=20 --normalize_peak=True --batch_size=1 --silence_threshold=0.1 --histograms=True --wavenet_params=./hiphop_3_4096_real_wavenet_params.json
+python train.py --data_dir=../sounds/hiphop --sample_size=96000 --logdir=./logdir/train/hiphop_16k_3_4096 --checkpoint_every=5000 --max_checkpoints=20 --batch_size=1 --silence_threshold=0.1 --histograms=True --wavenet_params=./hiphop_3_4096_wavenet_params.json
 
