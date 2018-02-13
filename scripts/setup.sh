70b575fe1cb64753c1a6fbff2510450d69a1d57f
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-02-13 23:21:54 +0000

    Better setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@
 set -e
 
 S3_REGION=${S3_REGION:-"us-west-2"}
+S3_ENDPOINT=${S3_ENDPOINT:-"s3.us-west-2.amazonaws.com"}
 
 pushd $HOME
 
@@ -12,7 +13,8 @@ sudo apt install -yy virtualenv libasound2-dev htop libcurl4-openssl-dev libssl-
 
 virtualenv -p /usr/bin/python3.5 tf
 
-echo "export S3_REGION=${S3_REGION}"
+echo "export S3_REGION=${S3_REGION}" >> ~/.bashrc
+echo "export S3_ENDPOINT=${S3_ENDPPOINT}" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64/:${LD_LIBRARY_PATH}" >> ~/.bashrc
 echo "export CUDA_HOME=/usr/local/cuda-9.0" >> ~/.bashrc
 echo "source ~/tf/bin/activate" >> ~/.bashrc
