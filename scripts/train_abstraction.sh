f1bb8dcc4dc1e36532dfaa76c95e0c326f2cec93
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-03-10 22:57:03 +0000

    Fix script

--- scripts/train_abstraction.sh
+++ scripts/train_abstraction.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+set -e
+
 cd "${0%/*}"
 
-MODEL_DIR=s3://wavenet-data/models/wavenet/abstraction_5x2_2048_4s
-OUTPUT_DIR=s3://wavenet-data/outputs/wavenet/abstraction_5x2_2048_4s
-TRAIN_PARAMS=--data_dir=../training/capacity --sample_size=64000 --logdir=${MODEL_DIR} --gausian_noise=0.01 --batch_size=2 --silence_threshold=0.1 --wavenet_params=../5_2048_double_wavenet_params.json --checkpoint_every 1000 --max_checkpoints=1000
-GENERATE_PARAMS=--samples=64000 --wavenet_params=../5_2048_double_wavenet_params.json --wav_seed ../../datasets/breakbeats/essential_breaks_00.wav --wav_out output.wav
+MODEL_DIR="s3://wavenet-data/models/wavenet/abstraction_5x2_2048_4s"
+OUTPUT_DIR="s3://wavenet-data/outputs/wavenet/abstraction_5x2_2048_4S"
+TRAIN_PARAMS="--data_dir=../training/capacity --sample_size=64000 --logdir=${MODEL_DIR} --gausian_noise=0.01 --batch_size=2 --silence_threshold=0.1 --wavenet_params=../5_2048_double_wavenet_params.json --checkpoint_every 1000 --max_checkpoints=1000"
+GENERATE_PARAMS="--samples=64000 --wavenet_params=../5_2048_double_wavenet_params.json --wav_seed ../../datasets/breakbeats/essential_breaks_00.wav --wav_out output.wav"
 
 mkdir -p ../training/abstraction
+rm -f ../training/abstraction/*.wav
 
 cp ../../datasets/breakbeats/essential_breaks_00.wav ../training/abstraction
 python ../train.py --num_steps=1000  ${TRAIN_PARAMS}
