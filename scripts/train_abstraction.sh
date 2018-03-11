fe4db3fe017b459e3c658bea250641f4a88b3fd5
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-03-11 14:28:33 +0000

    Run generation in parallel

--- scripts/train_abstraction.sh
+++ scripts/train_abstraction.sh
@@ -18,45 +18,50 @@ mkdir -p ../output
 
 cp ../../datasets/breakbeats/essential_breaks_00.wav ../training/abstraction
 ${TRAIN} --num_steps=500  ${TRAIN_PARAMS}
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8 &
+wait
 aws s3 sync ../output ${OUTPUT_DIR}/
 
 cp ../../datasets/breakbeats/essential_breaks_01.wav ../training/abstraction
 ${TRAIN} --num_steps=1500  ${TRAIN_PARAMS}
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8 &
+wait
 aws s3 sync ../output ${OUTPUT_DIR}/
 
 cp ../../datasets/breakbeats/essential_breaks_02.wav ../training/abstraction
 ${TRAIN} --num_steps=3000  ${TRAIN_PARAMS}
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8 &
+wait
 aws s3 sync ../output ${OUTPUT_DIR}/
 
 cp ../../datasets/breakbeats/essential_breaks_03.wav ../training/abstraction
 ${TRAIN} --num_steps=5000 ${TRAIN_PARAMS}
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-4999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8 &
+wait
 aws s3 sync ../output ${OUTPUT_DIR}/
 
 cp ../../datasets/breakbeats/essential_breaks_04.wav ../training/abstraction
 ${TRAIN} --num_steps=7500 ${TRAIN_PARAMS}
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
-CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7 &
+CUDA_VISIBLE_DEVICES="" ${GEN} ${MODEL_DIR}/model.ckpt-7499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8 &
+wait
 aws s3 sync ../output ${OUTPUT_DIR}/
