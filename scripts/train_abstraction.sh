6dddf01fcc2adb5d80c32e3528ed0ebec5be441e
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-03-11 13:26:59 +0000

    Add samples to abstraction study

--- scripts/train_abstraction.sh
+++ scripts/train_abstraction.sh
@@ -5,19 +5,58 @@ set -e
 cd "${0%/*}"
 
 MODEL_DIR="s3://wavenet-data/models/wavenet/abstraction_5x2_2048_4s"
-OUTPUT_DIR="s3://wavenet-data/outputs/wavenet/abstraction_5x2_2048_4S"
+OUTPUT_DIR="s3://wavenet-data/outputs/wavenet/abstraction_5x2_2048_4s"
 TRAIN_PARAMS="--data_dir=../training/abstraction --sample_size=64000 --logdir=${MODEL_DIR} --gausian_noise=0.01 --batch_size=2 --silence_threshold=0.1 --wavenet_params=../5_2048_double_wavenet_params.json --checkpoint_every 1000 --max_checkpoints=1000"
-GENERATE_PARAMS="--samples=64000 --wavenet_params=../5_2048_double_wavenet_params.json --wav_seed ../../datasets/breakbeats/essential_breaks_00.wav --wav_out output.wav"
+TRAIN="python ../train.py"
+GENERATE_PARAMS="--samples=64000 --wavenet_params=../5_2048_double_wavenet_params.json --wav_seed ../../datasets/breakbeats/essential_breaks_00.wav"
+GEN="CUDA_VISIBLE_DEVICES=\"\" python ../generate.py"
 
 mkdir -p ../training/abstraction
 rm -f ../training/abstraction/*.wav
 
+mkdir -p ../output
+
 cp ../../datasets/breakbeats/essential_breaks_00.wav ../training/abstraction
-python ../train.py --num_steps=1000  ${TRAIN_PARAMS}
-python ../generate.py ${MODEL_DIR}/model.ckpt-999 ${GENERATE_PARAMS}
-aws s3 cp output_999.wav ${OUTPUT_DIR}
+${TRAIN} --num_steps=1500  ${TRAIN_PARAMS}
+${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
+${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
+${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
+${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
+${GEN} ${MODEL_DIR}/model.ckpt-1499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+aws s3 sync ../output ${OUTPUT_DIR}/
 
 cp ../../datasets/breakbeats/essential_breaks_01.wav ../training/abstraction
-python ../train.py --num_steps=3000  ${TRAIN_PARAMS}
-python ../generate.py ${MODEL_DIR}/model.ckpt-2999 ${GENERATE_PARAMS}
-aws s3 cp output_2999.wav ${OUTPUT_DIR}
+${TRAIN} --num_steps=4500  ${TRAIN_PARAMS}
+${GEN} ${MODEL_DIR}/model.ckpt-4499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
+${GEN} ${MODEL_DIR}/model.ckpt-4499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
+${GEN} ${MODEL_DIR}/model.ckpt-4499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
+${GEN} ${MODEL_DIR}/model.ckpt-4499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
+${GEN} ${MODEL_DIR}/model.ckpt-4499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+aws s3 sync ../output ${OUTPUT_DIR}/
+
+cp ../../datasets/breakbeats/essential_breaks_02.wav ../training/abstraction
+${TRAIN} --num_steps=9000  ${TRAIN_PARAMS}
+${GEN} ${MODEL_DIR}/model.ckpt-8999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
+${GEN} ${MODEL_DIR}/model.ckpt-8999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
+${GEN} ${MODEL_DIR}/model.ckpt-8999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
+${GEN} ${MODEL_DIR}/model.ckpt-8999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
+${GEN} ${MODEL_DIR}/model.ckpt-8999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+aws s3 sync ../output ${OUTPUT_DIR}/
+
+cp ../../datasets/breakbeats/essential_breaks_03.wav ../training/abstraction
+${TRAIN} --num_steps=15000 ${TRAIN_PARAMS}
+${GEN} ${MODEL_DIR}/model.ckpt-14999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
+${GEN} ${MODEL_DIR}/model.ckpt-14999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
+${GEN} ${MODEL_DIR}/model.ckpt-14999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
+${GEN} ${MODEL_DIR}/model.ckpt-14999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
+${GEN} ${MODEL_DIR}/model.ckpt-14999 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+aws s3 sync ../output ${OUTPUT_DIR}/
+
+cp ../../datasets/breakbeats/essential_breaks_04.wav ../training/abstraction
+${TRAIN} --num_steps=22500 ${TRAIN_PARAMS}
+${GEN} ${MODEL_DIR}/model.ckpt-22499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.4.wav --temperature 0.4
+${GEN} ${MODEL_DIR}/model.ckpt-22499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.5.wav --temperature 0.5
+${GEN} ${MODEL_DIR}/model.ckpt-22499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.6.wav --temperature 0.6
+${GEN} ${MODEL_DIR}/model.ckpt-22499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.7.wav --temperature 0.7
+${GEN} ${MODEL_DIR}/model.ckpt-22499 ${GENERATE_PARAMS}  --wav_out ../output/output_0.8.wav --temperature 0.8
+aws s3 sync ../output ${OUTPUT_DIR}/
